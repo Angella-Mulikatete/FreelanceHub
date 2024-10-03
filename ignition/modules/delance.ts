373c5da64390edb667b3d9513c7083196d22473d
Angella-Mulikatete <mulikateteangella@gmail.com>
2024-10-03 23:46:31 +0300

    updated to hardhat

---
+++ ignition/modules/delance.ts
@@ -0,0 +1,10 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const DelanceModule = buildModule("DelanceModule", (m) => {
+
+    const delance = m.contract("Delance");
+
+    return { delance };
+});
+
+export default DelanceModule;
